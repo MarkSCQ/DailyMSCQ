d16576cfcd35bcb537ad0871195b986435a7ffc2
Mark233 <495431190@qq.com>
2021-07-29 00:31:32 -0700

    ''

--- Javascript/React/ReactNpm/ReactJSPang/hooksdemo02/demo2/src/Todo/index.jsx
+++ Javascript/React/ReactNpm/ReactJSPang/hooksdemo02/demo2/src/Todo/index.jsx
@@ -37,11 +37,23 @@ export default class Todo extends Component {
         })
         this.setState({ infos: new_infos })
     }
+    deleteIssues = (ct) => {
+        return () => {
+            console.log("@!!!", ct)
+            console.log("@ ", ct.key)
+            const new_infos = this.state.infos.filter(info => {
+                return info.id !== ct.key
+            })
+            this.setState({ infos: new_infos })
+        }
+    }
+    editProcess = (id) => {
+
+    }
 
     inputInfo = (field) => {
         return (event) => {
             const { target, keyCode } = event
-
             this.setState({
                 newinfo: {
                     ...this.state.newinfo,
@@ -63,25 +75,50 @@ export default class Todo extends Component {
     }
 
     resetNew = () => {
-        console.log(this.formRef)
-
         this.formRef.current.resetFields(["datepicker", "issueinput", "price"]);
         this.setState({ newinfo: { id: 0, issues: "", date: "", status: false, isNew: true } })
     }
 
     makeData = (data) => {
-        console.log("data is ", data)
-
         return data.map(info => {
             return { key: info.id, Issues: info.issues, Date: info.date, Price: info.price }
         })
     }
 
+    onClickRow = (record) => {
+        return {
+            onClick: () => {
+                this.setState({
+                    rowId: record.id,
+                });
+            },
+        };
+    }
+
+
+    setRowClassName = (record) => {
+        console.log(this.state.rowId)
+        console.log(record)
+        // console.log("@@",record)
+        // let issues = this.state.infos.filter(info=>{
+        //     return info.issues===record.Issues
+        // })[0].issues
+        // console.log("#F",issues)
+        // return record.Issues === issues? 'clickRowStyl' : 'clickRowStylBT';
+        const rowID = this.state.infos.find(info => {
+            return info.id === record.key
+        }).id
+        console.log(rowID)
+        console.log(record.key)
+        console.log(record.key === rowID)
+        return record.key === rowID ? 'clickRowStyl1' : 'clickRowStyl';
+
+    }
     submitForm = () => {
         // check if submission is valid
         const newInfo = this.state.newinfo
         // newinfo: { id: undefined, issues: "", price: 0, date: "", status: false, isNew: true },
-        if (newInfo.id === undefined || newInfo.issues === "") {
+        if (newInfo.id === undefined || newInfo.issues.length === 0) {
             this.resetNew()
             alert("invalid submission")
             return
@@ -103,7 +140,8 @@ export default class Todo extends Component {
                 //     sorter: (a, b) => a.age - b.age,
                 sorter: (a, b) => {
                     return new moment(a.Date).format('YYYYMMDD') - new moment(b.Date).format('YYYYMMDD')
-                }
+                },
+
             },
             {
                 title: 'Issues',
@@ -117,22 +155,42 @@ export default class Todo extends Component {
                 key: 'Price',
                 sorter: (a, b) => a.Price - b.Price,
             },
+            {
+                title: "Actions",
+                render: (text, record) => (
+                    <Space size="middle">
+                        <Button
+                            type="primary"
+                            style={{ width: "60px", height: "25px", fontSize: "10px", borderRadius: "8px", background: "white", borderColor: "#95de64" }}
+                        >
+                            <b style={{ color: "#95de64" }}>Edit</b>
+                        </Button>
+                        <Button
+                            onClick={this.deleteIssues(record)}
+                            danger
+                            style={{ width: "60px", height: "25px", fontSize: "10px", borderRadius: "8px" }}>
+                            <b>Delete</b>
+                        </Button>
+                    </Space>
+
+                ),
+            }
         ];
 
-        const data = this.makeData(this.state.infos)
+        const data = this.makeData(infos)
 
         return (
             <div style={{ margin: "50px" }}>
 
                 <Form
                     ref={this.formRef}
-                    labelCol={{ span: 8 }}
-                    wrapperCol={{ span: 16 }}>
+                    labelCol={{ span: 1 }}
+                    wrapperCol={{ span: 10 }}>
 
                     {/* <label><b>Date: &nbsp;&nbsp;</b></label> */}
                     <Form.Item
                         name="datepicker"
-                        label="Date"
+                        label={<b>Date</b>}
                     >
                         <DatePicker
                             style={{ width: "330px" }}
@@ -140,20 +198,20 @@ export default class Todo extends Component {
                         />
                     </Form.Item>
 
-                    <Form.Item name="issueinput" label="Issue">
+                    <Form.Item name="issueinput" label={<b>Issue</b>}>
                         <Input
                             style={{ width: "330px" }}
                             onKeyUp={this.inputInfo("issues")} />
                     </Form.Item>
 
 
-                    <Form.Item name="price" label="Price">
+                    <Form.Item name="price" label={<b>Price</b>}>
                         <Input
                             type="number"
                             style={{ width: "330px" }}
                             onKeyUp={this.inputInfo("price")} />
                     </Form.Item>
-                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Form.Item wrapperCol={{ offset: 1, span: 16 }}>
 
                         <Button
                             onClick={this.submitForm}
@@ -171,9 +229,17 @@ export default class Todo extends Component {
                     dataSource={data}
                     pagination={false}
                     onChange={this.onChangeTb}
-                    style={{ width: "600px" }}
+                    style={{ width: "700px" }}
+                    // rowClassName={this.setRowClassName}
+                    onRow={(record) => {
+                        return {
+                            onMouseEnter: event=>{
+                                console.log(event)
+                            }
+                        }
+                    }}
                 />
-            </div >
+            </div>
         )
     }
 }

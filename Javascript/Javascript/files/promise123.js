5d141e353cc5554af5a26cfcdfd34670fb9ac909
495431190@qq.com <495431190@qq.com>
2021-08-17 23:27:18 -0700

    promise-print
    promise print 1 2 3 per second.

---
+++ Javascript/Javascript/files/promise123.js
@@ -0,0 +1,20 @@
+const arr = [1, 2, 3]
+arr.reduce((p, x) => {
+    return p.then(() => {
+        return new Promise(r => {
+            setTimeout(() => r(console.log(x)), 1000)
+        })
+    })
+}, Promise.resolve())
+
+
+
+// const arr = [1, 2, 3]
+// arr.reduce((p, x) =>
+//     p.then(() =>
+//         new Promise(r =>
+//             setTimeout(
+//                 () => r(console.log(x)), 1000)
+//         )
+//     ), Promise.resolve()
+// )

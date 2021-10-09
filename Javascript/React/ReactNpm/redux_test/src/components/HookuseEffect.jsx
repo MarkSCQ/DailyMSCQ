f05aa25435897512e7b132cd65e6f097bb840e85
495431190@qq.com <495431190@qq.com>
2021-10-08 23:54:46 -0700

    uf

---
+++ Javascript/React/ReactNpm/redux_test/src/components/HookuseEffect.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+
+/**
+ * componentDidMount() componentDidUpdate() componentWillUpdate()
+ * can be used for updating state values
+ * can be used for applying side effects
+ *
+ * useEffect hook lets you perform side effects in functional components 
+ * it is a replacement for componentDidMount() componentDidUpdate() componentWillUpdate()
+ */
+export default function HookuseEffect() {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
